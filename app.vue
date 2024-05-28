6700ac98017a6b887062fb2d6ed858c22e5bbe78
Kostadin Draganov <kostadindraganov@gmail.com>
2024-05-28 14:44:57 +0300

    init

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    
-  </div>
-  <NuxtPage />
+	<div class="p-5 h-screen bg-gray-900 flex items-center justify-center">
+		<Button label="Click" />
+		<i class="pi pi-check" style="color: slateblue"></i>
+		<Button label="Check" icon="pi pi-check" class="m-5" />
+	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	devtools: { enabled: false },
+	modules: ["@nuxtjs/tailwindcss", "nuxt-primevue"],
+	primevue: {
+		cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+		components: {
+			exclude: ["Editor", "Chart"],
+		},
+	},
+	css: [
+		"primevue/resources/themes/lara-light-green/theme.css",
+		// "primevue/resources/themes/lara-dark-pink/theme.css",
+		// "primevue/resources/themes/lara-dark-indigo/theme.css",
+		"primeicons/primeicons.css",
+	],
+});
